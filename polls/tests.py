9995fc54c9643a1c68993662a157dce4ae571d67
veera-17 <veeragokulraj@testpress.in>
2024-09-27 16:58:23 +0530

    Completed upto Tutorial-6

--- polls/tests.py
+++ polls/tests.py
@@ -1,11 +1,11 @@
 import datetime
-from django.test import TestCase, TransactionTestCase
+from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
 from .models import Question
 
-class QuestionModelTests(TransactionTestCase):
+class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
